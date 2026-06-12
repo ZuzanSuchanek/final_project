928f5f018459eef656d5212d25cfaad5e3adc42d
ZuzanSuchanek <jakub.sykora@academicschool.cz>
2026-06-12 22:23:15 +0200

    MultipleChoiceQuestion byly přidány

--- final-final-project/trida2.cs
+++ final-final-project/trida2.cs
@@ -1,2 +1,30 @@
-namespace final_final_project;
+public class MultipleChoiceQuestion : Question
+{
+    public string[] Options { get; set; }
+    private string _correctAnswer;
+ 
+    public MultipleChoiceQuestion(string text, string[] options, string correctAnswer, int points)
+        : base(text, points)
+    {
+        Options = options;
+        _correctAnswer = correctAnswer.ToUpper();
+    }
+    
+    public override bool CheckAnswer(string answer)
+    {
+        return answer.Trim().ToUpper() == _correctAnswer;
+    }
+ 
+
+    public override void Display()
+    {
+        base.Display();
+ 
+        string[] labels = { "A", "B", "C", "D" };
+        for (int i = 0; i < Options.Length; i++)
+        {
+            Console.WriteLine($"   {labels[i]}) {Options[i]}");
+        }
+    }
+}
 
