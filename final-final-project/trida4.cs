3547ca94908ef0e29693859d501f0cf888ac22e7
ZuzanSuchanek <jakub.sykora@academicschool.cz>
2026-06-13 00:51:27 +0200

    quiz/smyčka

--- final-final-project/trida4.cs
+++ final-final-project/trida4.cs
@@ -1,6 +1,35 @@
-namespace final_final_project;
-
-public class trida4
+public class Quiz
 {
-    
+    public string Title { get; set; }
+
+    public Quiz(string title)
+    {
+        Title = title;
+    }
+
+    public void Start(Player player, List<Question> questions)
+    {
+        Console.WriteLine($"Vítej v kvízu: {Title}");
+        Console.WriteLine($"Hráč: {player.Name}\n");
+
+        foreach (Question question in questions)
+        {
+            question.Display();
+
+            Console.Write("Tvoje odpověď: ");
+            string answer = Console.ReadLine() ?? "";
+
+            if (question.CheckAnswer(answer))
+            {
+                Console.WriteLine($"Správně! +{question.Points} bodů\n");
+                player.AddPoints(question.Points);
+            }
+            else
+            {
+                Console.WriteLine("Špatně!\n");
+            }
+        }
+
+        Console.WriteLine($"Konec! {player.Name} získal {player.Score} bodů.");
+    }
 }
