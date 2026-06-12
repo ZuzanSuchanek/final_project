0f17c4ca258b863c7ed470e22b5c13df3d204554
ZuzanSuchanek <jakub.sykora@academicschool.cz>
2026-06-13 00:54:27 +0200

    program.cs + prejmenovaní trid

--- final-final-project/Program.cs
+++ final-final-project/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Zadej jméno: ");
+string name = Console.ReadLine() ?? "Trenér";
+Player player = new Player(name);
+
+List<Question> otazky = new List<Question>
+{
+    new MultipleChoiceQuestion("Jaký typ má Charmander?",
+        new[] { "Voda", "Tráva", "Oheň", "Elektřina" }, "C", 10),
+
+    new MultipleChoiceQuestion("Kolik Pokémonů je v původní Kanto Pokédex?",
+        new[] { "100", "151", "251", "386" }, "B", 10),
+
+    new MultipleChoiceQuestion("Kdo je evoluce Pikachu?",
+        new[] { "Raichu", "Pichu", "Jolteon", "Electabuzz" }, "A", 10),
+};
+
+Quiz quiz = new Quiz("Pokémon Kvíz");
+quiz.Start(player, otazky);
